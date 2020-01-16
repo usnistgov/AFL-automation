fc2743ae4745699ad054ccfe11f4c02ba072721e
Tyler Martin <tyler.martin@nist.gov>
2020-01-16 13:29:35 -0500

    Added json-key authetication to the roboto-client

--- NistoRoboto/RobotoClient.py
+++ NistoRoboto/RobotoClient.py
@@ -4,6 +4,18 @@ import requests
 class RobotoClient:
     def __init__(self,url = 'http://10.42.0.31:5000/'):
         self.url = url
+
+    def login(self,username):
+        kw = {}
+        kw['username'] = username
+        kw['password'] = 'domo_arigato'
+        reply = requests.post(self.url+'/login',json=kw)
+        if not (reply.status_code == 200):
+            raise RuntimeError(f'RobotoClient Login failed with error code {reply.status_code}')
+
+        self.token  = reply.json()['token']
+        self.headers = {'Authorization':'Bearer {}'.format(self.token)}
+
     def transfer(self,mount,source,dest,volume):
         '''Transfer fluid from one location to another
 
@@ -24,9 +36,8 @@ class RobotoClient:
 
         '''
         kw = {}
-        kw['key'] = NistoRoboto.RobotoFlaskKey.__flaskkey__
         kw['mount'] = mount
         kw['source'] = source
         kw['dest'] = dest
         kw['volume'] = volume
-        print(requests.post(self.url+'/transfer',json=kw))
+        print(requests.post(self.url+'/transfer',headers=self.headers,json=kw))
--- NistoRoboto/RobotoFlaskApp.py
+++ NistoRoboto/RobotoFlaskApp.py
@@ -1,6 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask import request, jsonify
+import requests
 
+from flask_jwt_extended import (
+    JWTManager, jwt_required, create_access_token,
+    get_jwt_identity
+)
 
 import NistoRoboto.RobotoFlaskKey
 def key_is_valid(key):
@@ -14,6 +19,10 @@ roboto_server = RobotoServer()
 
 app = Flask(__name__)
 
+# maybe hide this at some point?
+app.config['JWT_SECRET_KEY'] = '03570'  
+jwt = JWTManager(app)
+
 @app.route('/')
 def index():
     '''
@@ -25,16 +34,40 @@ def index():
     - loaded labeware
     - visualization of the deck
     '''
-    return 'Cool status and visualizations to come...'
+    kw = {}
+    kw['pipettes'] = None#roboto_server.protocol.loaded_pipettes
+    kw['labware']  = None#roboto_server.protocol.loaded_labware
+
+    #request image and save to static directory
+    #reply = requests.post('http://localhost:31950/camer/picture')
+
+    return render_template('index.html',**kw)
+
+@app.route('/login',methods=['POST'])
+def login():
+    if not request.is_json:
+        return jsonify({"msg": "Missing JSON in request"}), 400
+
+    username = request.json.get('username', None)
+    password = request.json.get('password', None)
+    if not username:
+        return jsonify({"msg": "Missing username parameter"}), 400
+    if not password:
+        return jsonify({"msg": "Missing password parameter"}), 400
+
+    if password != 'domo_arigato':
+        return jsonify({"msg": "Bad password"}), 401
+
+    # Identity can be any data that is json serializable
+    token = create_access_token(identity=username)
+    return jsonify(token=token), 200
+
 
 @app.route('/transfer',methods=['POST'])
+@jwt_required
 def transfer():
-    if not (('key' in request.json) and key_is_valid(request.json['key'])):
-        print('Basic server key-check failed')
-        return 'Failed'
     roboto_server.transfer(**request.json)
-    return 'Success'
-
+    return 'Success',200
 
 
 if __name__ == '__main__':

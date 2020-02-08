cae5742e43ce5abfa55d82a9ecd0e6cbbce872b4
Tyler Martin <tyler.martin@nist.gov>
2020-02-08 12:50:06 -0500

    Generic Client for DeviceServer
    Client can be subclassed for specific device pools or used directly

--- NistoRoboto/DeviceServer/Client.py
+++ NistoRoboto/DeviceServer/Client.py
@@ -1,10 +1,10 @@
 import requests
 
 class Client:
-    '''Communicate with NistoRoboto server on OT-2
+    '''Communicate with DeviceServer 
 
     This class maps pipettor functions to HTTP REST requests that are sent to
-    the NistoRoboto server
+    the server
     '''
     def __init__(self,ip='10.42.0.30',port='5000'):
         #trim trailing slash if present
@@ -16,7 +16,7 @@ class Client:
 
     def logged_in(self):
         url = self.url + '/login_test'
-        response = requests.post(url,headers=self.headers)
+        response = requests.get(url,headers=self.headers)
         if response.status_code == 200:
             return True
         else:
@@ -33,73 +33,31 @@ class Client:
         self.token  = response.json()['token']
         self.headers = {'Authorization':'Bearer {}'.format(self.token)}
 
-    def set_queue_mode(self,debug_mode=True):
-        json={'debug_mode':debug_mode}
+    def enqueue(self,**kwargs):
+        json=kwargs
         response = requests.post(self.url+'/enqueue',headers=self.headers,json=json)
         if response.status_code != 200:
             raise RuntimeError(f'API call to set_queue_mode command failed with status_code {response.status_code}\n{response.text}')
-
-    def transfer(self,source,dest,volume):
-        '''Transfer fluid from one location to another
-
-        Arguments
-        ---------
-        source: str or list of str
-            Source wells to transfer from. Wells should be specified as three
-            character strings with the first character being the slot number.
-
-        dest: str or list of str
-            Destination wells to transfer from. Wells should be specified as
-            three character strings with the first character being the slot
-            number.
-
-        volume: float
-            volume of fluid to transfer in microliters
-
-        '''
-        json = {}
-        json['type']  = 'transfer'
-        json['source'] = source
-        json['dest']   = dest
-        json['volume'] = volume
-        response = requests.post(self.url+'/enqueue',headers=self.headers,json=json)
+        
+    def pause(self,state):
+        json={'state':state}
+        response = requests.post(self.url+'/pause',headers=self.headers,json=json)
         if response.status_code != 200:
-            raise RuntimeError(f'API call to transfer command failed with status_code {response.status_code}\n{response.content}')
+            raise RuntimeError(f'API call to set_queue_mode command failed with status_code {response.status_code}\n{response.text}')
 
-    def load_labware(self,name,slot):
-        json = {}
-        json['type']  = 'load_labware'
-        json['name'] = name
-        json['slot'] = slot
-        response = requests.post(self.url+'/enqueue',headers=self.headers,json=json)
+    def debug_mode(self,state):
+        json={'state':state}
+        response = requests.post(self.url+'/debug_mode',headers=self.headers,json=json)
         if response.status_code != 200:
-            raise RuntimeError(f'API call to load_labware command failed with status_code {response.status_code}\n{response.content}')
-
-    def load_instrument(self,name,mount,tip_rack_slots):
-        json = {}
-        json['type']  = 'load_instrument'
-        json['name'] = name
-        json['mount'] = mount
-        json['tip_rack_slots'] = tip_rack_slots
-        response = requests.post(self.url+'/enqueue',headers=self.headers,json=json)
-        if response.status_code != 200:
-            raise RuntimeError(f'API call to load_instrument command failed with status_code {response.status_code}\n{response.content}')
-
-    def reset(self):
-        json = {}
-        json['type']  = 'reset'
-        response = requests.post(self.url+'/enqueue',headers=self.headers,json=json)
-        if response.status_code != 200:
-            raise RuntimeError(f'API call to reset command failed with status_code {response.status_code}\n{response.content}')
-
-    def home(self):
-        json = {}
-        json['type']  = 'home'
-        response = requests.post(self.url+'/enqueue',headers=self.headers,json=json)
-        if response.status_code != 200:
-            raise RuntimeError(f'API call to home command failed with status_code {response.status_code}\n{response.content}')
+            raise RuntimeError(f'API call to set_queue_mode command failed with status_code {response.status_code}\n{response.text}')
 
     def halt(self):
         response = requests.post(self.url+'/halt',headers=self.headers,json={})
         if response.status_code != 200:
             raise RuntimeError(f'API call to halt command failed with status_code {response.status_code}\n{response.content}')
+
+    def queue_state(self):
+        response = requests.post(self.url+'/queue_state',headers=self.headers,json={})
+        if response.status_code != 200:
+            raise RuntimeError(f'API call to queue_state command failed with status_code {response.status_code}\n{response.content}')
+        return response

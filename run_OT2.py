7d60389d8fed8e4e14778ad00ad96d395da1ca25
RobotoStation <tbm@nist.gov>
2020-02-09 17:36:52 -0500

    bugfix in run_OT2.sh

--- run_OT2.py
+++ run_OT2.py
@@ -1,9 +1,7 @@
 from NistoRoboto.DeviceServer.DeviceServer import DeviceServer
 from NistoRoboto.DeviceServer.OT2Protocol import OT2Protocol
 import os
-server = DeviceServer('OT2Server',
-           root_path=os.getcwd()+'/NistoRoboto/DeviceServer/',
-           )
+server = DeviceServer('OT2Server')
 protocol = OT2Protocol()
 server.add_standard_routes()
 server.create_queue(protocol)

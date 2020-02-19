dfff7ee331e62f2e2425fdd61c97a14b8e6399ea
RobotoStation <tbm@nist.gov>
2020-02-19 18:43:09 -0500

    Attempting to add protocol output to SpecScreen

--- NistoRoboto/DeviceServer/SpecScreenProtocol.py
+++ NistoRoboto/DeviceServer/SpecScreenProtocol.py
@@ -4,9 +4,33 @@ from math import ceil,sqrt
 import subprocess,shlex
 
 class SpecScreenProtocol(Protocol):
-    def __init__(self):
+    def __init__(self,log_file=None):
         self.app = None
         self.name = 'SpecScreenProtocol'
+        self.log_file = log_file
+        self.n_spec_blocks=3
+
+    def status(self):
+        status = []
+        if self.log_file is not None:
+            f = subprocess.Popen(shlex.split('tail -n 1000 {self.log_file}'),stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+
+            lines = []
+            spec_lines = []
+            for i,line in enumerate(f.stdout.readline()):
+                lines.append(line)
+                if 'SPEC' in line:
+                    spec_lines.append(i)
+
+            
+            spec_lines = spec_lines[::-1]
+            for i in range(self.n_spec_blocks): 
+                m = spec_lines[i]
+                n = spec_lines[i+1]
+                status.append('\n'.join(lines[m:n]))
+            status = status[::-1]
+
+        return status
         
     def execute(self,**kwargs):
         if 'spec_cmd' not in kwargs:
--- server_scripts/SpecScreen.py
+++ server_scripts/SpecScreen.py
@@ -2,9 +2,8 @@ import os
 from NistoRoboto.DeviceServer.DeviceServer import DeviceServer
 from NistoRoboto.DeviceServer.SpecScreenProtocol import SpecScreenProtocol
 
-SpecScreen = SpecScreenProtocol()
-
+protocol = SpecScreenProtocol(log_file='/mnt/home/chess_id3b/currentdaq/beaucage-1021-1/beaucage-1021-1.tlog')
 server = DeviceServer('SpecScreenServer')
 server.add_standard_routes()
-server.create_queue(SpecScreen)
+server.create_queue(protocol)
 server.run(host='0.0.0.0', debug=False)

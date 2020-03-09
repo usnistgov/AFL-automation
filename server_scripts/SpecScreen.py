6a52ca4feb79bba51010da0ca08ee161c7fc95e8
Peter Beaucage <github@peterbeaucage.com>
2020-03-09 12:07:41 -0400

    Fixed path resolution for server scripts and added chrome instance tied to server.

--- server_scripts/SpecScreen.py
+++ server_scripts/SpecScreen.py
@@ -1,4 +1,12 @@
-import os
+import os,sys,subprocess
+
+try:
+	import NistoRoboto
+except:
+	sys.path.append('../')
+
+server_port=5000
+
 from NistoRoboto.DeviceServer.DeviceServer import DeviceServer
 from NistoRoboto.DeviceServer.SpecScreenProtocol import SpecScreenProtocol
 
@@ -6,4 +14,10 @@ protocol = SpecScreenProtocol(log_file='/mnt/home/chess_id3b/currentdaq/beaucage
 server = DeviceServer('SpecScreenServer')
 server.add_standard_routes()
 server.create_queue(protocol)
-server.run(host='0.0.0.0', debug=False)
+server.run(host='0.0.0.0', port=server_port, debug=False)
+
+process = subprocess.Popen(f'chromium-browser --start-fullscreen http://localhost:{server_port}', shell=True, stdout=subprocess.PIPE)
+process.wait()
+
+server.stop()
+server.join()

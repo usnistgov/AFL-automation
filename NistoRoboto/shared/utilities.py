6ec386804b529213f4aedadf74294c2d373b9687
Tyler Martin <tyler.martin@nist.gov>
2020-09-28 16:59:02 -0400

    Utility to print with timestamps

--- NistoRoboto/shared/utilities.py
+++ NistoRoboto/shared/utilities.py
@@ -5,3 +5,8 @@ def listify(obj):
     if isinstance(obj, str) or not hasattr(obj, "__iter__"):
         obj = [obj]
     return obj
+
+def tprint(in_str):
+    now = datetime.datetime.now().strftime('%y%m%d-%H:%M:%S')
+    print(f'[{now}] {in_str}')
+

12bd67157a4f1bf5a427af6d17311a47b0997c65
RobotoStation <tbm@nist.gov>
2020-09-28 20:03:21 -0400

    Revert "Made enqueue push to back rather than front of queue by default"
    This reverts commit df2ed8a5652dd955c640bbc4fbc4e3c5d81f5359.

--- NistoRoboto/DeviceServer/DeviceServer.py
+++ NistoRoboto/DeviceServer/DeviceServer.py
@@ -116,7 +116,7 @@ class DeviceServer:
             queue_loc = task['queue_loc']
             del task['queue_loc']
         else:
-            queue_loc=0
+            queue_loc=task_queue.qsize()
 
         user = get_jwt_identity()
         self.app.logger.info(f'{user} enqueued {request.json}')

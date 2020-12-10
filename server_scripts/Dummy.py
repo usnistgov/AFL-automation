7f01d6f751f4d68bebcfe341da87b84c6c203640
Tyler Martin <tyler.martin@nist.gov>
2020-12-09 20:57:08 -0500

    Rolling back to three server Dummy

--- server_scripts/Dummy.py
+++ server_scripts/Dummy.py
@@ -13,28 +13,28 @@ from NistoRoboto.APIServer.driver.NICE_SampleDriver import NICE_SampleDriver
 server = APIServer('DummyPumpServer',index_template="index_pump.html")
 server.add_standard_routes()
 server.create_queue(DummyDriver(name='DummyPump'))
-#server.run_threaded(host='0.0.0.0', port=5051, debug=False)
-server.init_logging()
-server.run(host='0.0.0.0', port=5051, debug=True)
-
-# server = APIServer('DummyOT2Server')
-# server.add_standard_routes()
-# server.create_queue(DummyDriver(name='DummyOT2'))
-# server.run_threaded(host='0.0.0.0', port=5052, debug=False)
-# 
-# driver =NICE_SampleDriver(
-#         nice_url=None,
-#         load_url='localhost:5051',
-#         prep_url='localhost:5052',
-#         camera_urls = [
-#             # 'http://picam:8081/1/current',
-#             #'http://picam:8081/2/current',
-#             #'http://picam:8081/3/current',
-#             ]
-#         )
-# 
-# server = APIServer('DummySampleServer')
-# server.add_standard_routes()
-# server.create_queue(driver)
+server.run_threaded(host='0.0.0.0', port=5051, debug=False)
 # server.init_logging()
-# server.run(host='0.0.0.0',port=5050, debug=False)
+# server.run(host='0.0.0.0', port=5051, debug=True)
+
+server = APIServer('DummyOT2Server')
+server.add_standard_routes()
+server.create_queue(DummyDriver(name='DummyOT2'))
+server.run_threaded(host='0.0.0.0', port=5052, debug=False)
+
+driver =NICE_SampleDriver(
+        nice_url=None,
+        load_url='localhost:5051',
+        prep_url='localhost:5052',
+        camera_urls = [
+            # 'http://picam:8081/1/current',
+            #'http://picam:8081/2/current',
+            #'http://picam:8081/3/current',
+            ]
+        )
+
+server = APIServer('DummySampleServer')
+server.add_standard_routes()
+server.create_queue(driver)
+server.init_logging()
+server.run(host='0.0.0.0',port=5050, debug=False)

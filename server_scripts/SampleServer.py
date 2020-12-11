a94d756d68474ba210bd32ebdcd18f518588c16a
RobotoStation <tbm@nist.gov>
2020-12-11 17:49:22 -0500

    Minor tweaks to server_scripts

--- server_scripts/SampleServer.py
+++ server_scripts/SampleServer.py
@@ -13,16 +13,15 @@ from NistoRoboto.APIServer.driver.NICE_SampleDriver import NICE_SampleDriver
 
 driver =NICE_SampleDriver(
         nice_url='NGBSANS.ncnr.nist.gov',
-        load_url='piloader:5000',
+        load_url='piloader2:5000',
         prep_url='piot2:5000',
         camera_urls = [
-            # 'http://picam:8081/1/current',
-            'http://picam:8081/2/current',
-            'http://picam:8081/3/current',
+            'http://robocam:8081/101/current',
+            'http://robocam:8081/102/current',
             ]
         )
 server = APIServer('SampleServer')
 server.add_standard_routes()
 server.create_queue(driver)
-server.init_logging(toaddrs=['tyler.martin@nist.gov'])
+server.init_logging(toaddrs=['tyler.martin@nist.gov','peter.beaucage@nist.gov','beaucage.peter@gmail.com'])
 server.run(host='0.0.0.0',port=5000, debug=False)

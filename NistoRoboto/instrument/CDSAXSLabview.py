785bfc67271bf833893040fc9192332b9269048b
Peter Beaucage <github@peterbeaucage.com>
2021-02-20 12:48:38 -0500

    Add online reduction support for live-data

--- NistoRoboto/instrument/CDSAXSLabview.py
+++ NistoRoboto/instrument/CDSAXSLabview.py
@@ -3,8 +3,10 @@ import win32com.client
 import pythoncom
 import time
 from NistoRoboto.APIServer.driver.Driver import Driver
+from NistoRoboto.instrument.ScatteringInstrument import ScatteringInstrument
 
-class CDSAXSLabview(Driver):
+
+class CDSAXSLabview(ScatteringInstrument,Driver):
     
     axis_name_to_id_lut = {
         'X-stage' : 0,
@@ -23,7 +25,7 @@ class CDSAXSLabview(Driver):
     
     axis_id_to_name_lut = {value:key for key, value in axis_name_to_id_lut.items()}
     
-    def __init__(self,vi=r'C:\saxs_control\GIXD controls.vi'):
+    def __init__(self,vi=r'C:\saxs_control\GIXD controls.vi',**kwargs):
         '''
         connect to locally running labview vi with win32com and 
 
@@ -34,19 +36,8 @@ class CDSAXSLabview(Driver):
 
         self.app = None
         self.name = 'CDSAXSLabview'
-        self.vi = vi
         
-        pythoncom.CoInitialize()
-        self.labview = win32com.client.dynamic.Dispatch("Labview.Application")
-
-        self.main_vi = self.labview.getvireference(self.vi)
-
-        self.main_vi.setcontrolvalue('Measurement',3) # 3 should bring the single Pilatus tab to the front
-
-    def connect(self):
-        self.labview = win32com.client.dynamic.Dispatch("Labview.Application")
-        self.main_vi = self.labview.getvireference(self.vi)
-        pythoncom.CoInitialize()
+        super.__init__(**kwargs)
         
     @Driver.unqueued()        
     def getExposure(self):
@@ -54,33 +45,34 @@ class CDSAXSLabview(Driver):
             get the currently set exposure time
 
         '''
-        self.connect()
-        return self.main_vi.getcontrolvalue('Single Pilatus Parameters')[0]
+        with LabviewConnection() as lv:
+            return lv.main_vi.getcontrolvalue('Single Pilatus Parameters')[0]
+
     def getFilename(self):
         '''
             get the currently set file name
 
         '''
-        self.connect()
-        return self.main_vi.getcontrolvalue('Single Pilatus Parameters')[1]
+        with LabviewConnection() as lv:
+            return lv.self.main_vi.getcontrolvalue('Single Pilatus Parameters')[1]
 
     @Driver.unqueued()
     def printMainVi(self):
-        self.connect()
-        return str(self.main_vi)
+        with LabviewConnection() as lv:
+            return str(lv.main_vi)
     @Driver.unqueued()
     def printLabviewObj(self):
-        self.connect()
-        return str(self.labview)
+         with LabviewConnection() as lv:
+            return str(lv.labview)
     
     def setExposure(self,exposure):
         if self.app is not None:
             self.app.logger.debug(f'Setting exposure time to {exposure}')
-        self.connect()
-        
+                
         fileName = self.getFilename()
 
-        self.main_vi.setcontrolvalue('Single Pilatus Parameters',(exposure,fileName))
+        with LabviewConnection() as lv:
+            lv.main_vi.setcontrolvalue('Single Pilatus Parameters',(exposure,fileName))
 
     def setFilename(self,name):
         if self.app is not None:
@@ -91,82 +83,84 @@ class CDSAXSLabview(Driver):
             
         exposure = self.getExposure()
 
-        self.main_vi.setcontrolvalue('Single Pilatus Parameters',(exposure,name))
+        with LabviewConnection() as lv:
+            lv.main_vi.setcontrolvalue('Single Pilatus Parameters',(exposure,name))
 
 
     def getPath(self):
-        return self.main_vi.getcontrolvalue('FilePath')   # @TODO: fill in here
+        with LabviewConnection() as lv:
+            return lv.main_vi.getcontrolvalue('FilePath')   # @TODO: fill in here
     
     @Driver.unqueued(render_hint='2d_img',log_image=True)
     def getData(self,**kwargs):
-        self.connect()
-        return self.main_vi.getcontrolvalue('Pilatus Data')
+        with LabviewConnection() as lv:
+            return lv.main_vi.getcontrolvalue('Pilatus Data')
 
     def setPath(self,path):
         if self.app is not None:
             self.app.logger.debug(f'Setting file path to {path}')
 
         self.path = str(path)
-        self.main_vi.setcontrolvalue('FilePath',path)
+        with LabviewConnection() as lv:
+            lv.main_vi.setcontrolvalue('FilePath',path)
         
     @Driver.unqueued()  
     def getStatus(self):
-        self.connect()
-        return self.main_vi.getcontrolvalue('Process Status Message')
+        with LabviewConnection() as lv:
+            return lv.main_vi.getcontrolvalue('Process Status Message')
    
     @Driver.unqueued()  
     def getNScans(self):
-        self.connect()
-        return self.main_vi.getcontrolvalue('# of Scans')
+        with LabviewConnection() as lv:
+            return lv.main_vi.getcontrolvalue('# of Scans')
         
     def setNScans(self,nscans):
-        self.connect()
-        self.main_vi.setcontrolvalue('# of Scans',nscans)
+        with LabviewConnection() as lv:
+            lv.main_vi.setcontrolvalue('# of Scans',nscans)
     
     @Driver.unqueued()  
     def getSweepAxis(self):
-        self.connect()
-        return self.axis_id_to_name_lut[self.main_vi.getcontrolvalue('Sweep Axis')]
+        with LabviewConnection() as lv:
+            return self.axis_id_to_name_lut[lv.main_vi.getcontrolvalue('Sweep Axis')]
     def setSweepAxis(self,axis):
         if type(axis)=='str':
             axis=self.axis_name_to_id_lut[axis]
-        self.connect()
-        self.main_vi.setcontrolvalue('Sweep Axis',axis) # this is an enum, 6 is Y-stage
+        with LabviewConnection() as lv:
+            lv.main_vi.setcontrolvalue('Sweep Axis',axis) # this is an enum, 6 is Y-stage
     
     @Driver.unqueued()  
     def getYStagePos(self):
-        self.connect()
-        return self.main_vi.getcontrolvalue('Y-stage')
+        with LabviewConnection() as lv:
+            return lv.main_vi.getcontrolvalue('Y-stage')
     @Driver.unqueued()  
     def getZStagePos(self):
-        self.connect()
-        return self.main_vi.getcontrolvalue('Z-stage')
+        with LabviewConnection() as lv:
+            return lv.main_vi.getcontrolvalue('Z-stage')
     @Driver.unqueued()  
     def getXStagePos(self):
-        self.connect()
-        return self.main_vi.getcontrolvalue('X-Stage')
+        with LabviewConnection() as lv:
+            return lv.main_vi.getcontrolvalue('X-Stage')
     
     
     def setSweepStart(self,start):
-        self.connect()
-        self.main_vi.setcontrolvalue('Start Value',start)
+        with LabviewConnection() as lv:
+            lv.main_vi.setcontrolvalue('Start Value',start)
         
     @Driver.unqueued()  
     def getSweepStart(self):
-        self.connect()
-        return self.main_vi.getcontrolvalue('Start Value')
+        with LabviewConnection() as lv:
+            return lv.main_vi.getcontrolvalue('Start Value')
     
     def setSweepStep(self,step):
-        self.connect()
-        self.main_vi.setcontrolvalue('Step',step)
+        with LabviewConnection() as lv:
+            lv.main_vi.setcontrolvalue('Step',step)
     
     @Driver.unqueued()  
     def getSweepStep(self):
-        self.connect()
-        return self.main_vi.getcontrolvalue('Step')
+        with LabviewConnection() as lv:
+            return lv.main_vi.getcontrolvalue('Step')
         
     def expose(self,name=None,exposure=None,block=False):
-        self.connect()
         if name is not None:
             self.setFilename(name)
         else:
@@ -184,14 +178,14 @@ class CDSAXSLabview(Driver):
         if self.app is not None:
             self.app.logger.debug(f'Starting exposure with name {name} for {exposure} s')
 
-        self.main_vi.setcontrolvalue('Expose Pilatus',True)
+        with LabviewConnection() as lv:
+            lv.main_vi.setcontrolvalue('Expose Pilatus',True)
         time.sleep(0.5)
         if block:
             while(self.getStatus() != 'Success'):
                 time.sleep(0.1)
                 
     def scan(self,axis,npts,start,step,name=None,exposure=None,block=False):
-        self.connect()
         if name is not None:
             self.setFilename(name)
         else:
@@ -210,8 +204,35 @@ class CDSAXSLabview(Driver):
         if self.app is not None:
             self.app.logger.debug(f'Starting exposure with name {name} for {exposure} s')
 
-        self.main_vi.setcontrolvalue('Expose Pilatus',True)
+        with LabviewConnection() as lv:
+            lv.main_vi.setcontrolvalue('Expose Pilatus',True)
         time.sleep(0.5)
         if block:
             while(self.getStatus() != 'Success'):
                 time.sleep(0.1)
+
+class LabviewConnection():
+
+    def __init__(self,vi=r'C:\saxs_control\GIXD controls.vi'):
+        '''
+        connect to locally running labview vi with win32com and 
+
+        parameters:
+            vi: (str) the path to the LabView virtual instrument file for the main interface
+
+        '''
+
+        self.vi = vi
+        
+
+    def __enter__(self):
+        pythoncom.CoInitialize()
+        self.labview = win32com.client.dynamic.Dispatch("Labview.Application")
+        self.main_vi = self.labview.getvireference(self.vi)
+        self.main_vi.setcontrolvalue('Measurement',3) # 3 should bring the single Pilatus tab to the front
+        return self
+
+    def __exit__(self):
+        pythoncom.CoUnInitialize()
+        self.labview=None
+        self.main_vi=None
--- NistoRoboto/instrument/ScatteringInstrument.py
+++ NistoRoboto/instrument/ScatteringInstrument.py
@@ -1,11 +1,21 @@
+from NistoRoboto.APIServer.Driver import Driver
+import pyFAI, pyFAI.azimuthalIntegrator
+import numpy as np
+from PIL import Image
+
 
 
 class ScatteringInstrument():
 
-    def __init__(self,exposuretime=1,thickness=1,nexp=1):
-            self.exposuretime = exposuretime
-            self.thickness = thickness
-            self.nexp = nexp
+    def __init__(self,
+    			reduction_params={'poni1':0,'poni2':0,'rot1':0,'rot2':0,'rot3':0,'wavelength':0,'dist':0},
+    			detector_name='pilatus300kw',
+    			mask_path=None,
+    			):
+    	self.reduction_params = reduction_params
+    	self.detector_name = detector_name
+    	self.mask_path = mask_path
+    	self.generateIntegrator()
 
     def cell_in_beam(self,cellid):
 
@@ -15,7 +25,63 @@ class ScatteringInstrument():
 
         raise NotImplementedError
 
-    
+    def generateIntegrator(self):
+    	self.detector = pyFAI.detector_factory(name=self.detector_name)
+        if(self.mask_path is None):
+            self.mask=None
+       	else:
+       	    self.mask = np.array(Image.open(mask_path))
+        self.integrator = pyFAI.azimuthalIntegrator.AzimuthalIntegrator(detector=self.detector,
+                                                                        wavelength = self.reduction_params['wavelength']
+                                                                        dist = self.reduction_params['dist'],
+                                                                        poni1 = self.reduction_params['poni1'],
+                                                                        poni2 = self.reduction_params['poni2'],
+                                                                        rot1 = self.reduction_params['rot1'],
+                                                                        rot2 = self.reduction_params['rot2'],
+                                                                        rot3 = self.reduction_params['rot3'],
+                                                                        )
+
+    def setReductionParams(self,reduction_params):
+    	self.reduction_params.update(reduction_params)
+    	self.generateIntegrator()
+
+    def setMaskPath(self,mask_path):
+        self.mask_path = mask_path
+        self.generateIntegrator()
+
+    def setDetectorName(self,detector_name):
+    	self.detector_name=detector_name
+
+    @Driver.unqueued()
+    def getReductionParams(self):
+    	return self.reduction_params
+
+    @Driver.unqueued()
+    def getMaskPath(self):
+    	return self.mask_path
+
+    @Driver.unqueued()
+    def getDetectorName(self):
+    	return self.detector_name
+
+    @Driver.unqueued(render_hint='1d_plot',xlin=False,ylin=False,xlabel='q (A^-1)',ylabel='Intensity (AU)')
+    def getReducedData(self,reduce_type='1d',**kwargs):
+    	img = self.getData(**kwargs)
+
+    	if self.mask is None:
+            mask = np.zeros(np.shape(data))
+        else:
+            mask = self.mask
+
+        if reduce_type == '1d':
+        	res = self.integrator.integrate1d(img,self.npts,unit='q_A^-1',mask=mask,error_model='azimuthal')
+        elif reduce_type == '2d':
+       		res = self.integrator.integrate2d(img,self.npts,unit='q_A^-1',mask=mask,error_model='azimuthal')
+        else:
+        	raise ValueError('unsupported return_type')
+
+        return res
+
 
 
 

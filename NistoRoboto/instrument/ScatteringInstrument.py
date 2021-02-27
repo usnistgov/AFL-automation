1a565aae6485b1e5cff300b8d37e547410d4d077
Peter Beaucage <github@peterbeaucage.com>
2021-02-27 09:39:17 -0500

    Runtime tweaks to ScatteringInstrument, profiling, use fabio for mask loading

--- NistoRoboto/instrument/ScatteringInstrument.py
+++ NistoRoboto/instrument/ScatteringInstrument.py
@@ -1,21 +1,22 @@
-from NistoRoboto.APIServer.Driver import Driver
+from NistoRoboto.APIServer.driver.Driver import Driver
 import pyFAI, pyFAI.azimuthalIntegrator
 import numpy as np
 from PIL import Image
-
+import datetime
+import fabio
 
 
 class ScatteringInstrument():
 
     def __init__(self,
-    			reduction_params={'poni1':0,'poni2':0,'rot1':0,'rot2':0,'rot3':0,'wavelength':0,'dist':0},
-    			detector_name='pilatus300kw',
-    			mask_path=None,
-    			):
-    	self.reduction_params = reduction_params
-    	self.detector_name = detector_name
-    	self.mask_path = mask_path
-    	self.generateIntegrator()
+                reduction_params={'poni1':0,'poni2':0,'rot1':0,'rot2':0,'rot3':0,'wavelength':1.3421e-10,'dist':2.5,'npts':500},
+                detector_name='pilatus300kw',
+                mask_path=None,
+                ):
+        self.reduction_params = reduction_params
+        self.detector_name = detector_name
+        self.mask_path = mask_path
+        self.generateIntegrator()
 
     def cell_in_beam(self,cellid):
 
@@ -26,13 +27,13 @@ class ScatteringInstrument():
         raise NotImplementedError
 
     def generateIntegrator(self):
-    	self.detector = pyFAI.detector_factory(name=self.detector_name)
+        self.detector = pyFAI.detector_factory(name=self.detector_name)
         if(self.mask_path is None):
             self.mask=None
-       	else:
-       	    self.mask = np.array(Image.open(mask_path))
+        else:
+           self.mask = fabio.open(self.mask_path).data
         self.integrator = pyFAI.azimuthalIntegrator.AzimuthalIntegrator(detector=self.detector,
-                                                                        wavelength = self.reduction_params['wavelength']
+                                                                        wavelength = self.reduction_params['wavelength'],
                                                                         dist = self.reduction_params['dist'],
                                                                         poni1 = self.reduction_params['poni1'],
                                                                         poni2 = self.reduction_params['poni2'],
@@ -42,44 +43,56 @@ class ScatteringInstrument():
                                                                         )
 
     def setReductionParams(self,reduction_params):
-    	self.reduction_params.update(reduction_params)
-    	self.generateIntegrator()
+        self.reduction_params.update(reduction_params)
+        self.generateIntegrator()
 
     def setMaskPath(self,mask_path):
         self.mask_path = mask_path
         self.generateIntegrator()
 
     def setDetectorName(self,detector_name):
-    	self.detector_name=detector_name
+        self.detector_name=detector_name
 
     @Driver.unqueued()
     def getReductionParams(self):
-    	return self.reduction_params
+        return self.reduction_params
 
     @Driver.unqueued()
     def getMaskPath(self):
-    	return self.mask_path
+        return self.mask_path
 
     @Driver.unqueued()
     def getDetectorName(self):
-    	return self.detector_name
+        return self.detector_name
 
     @Driver.unqueued(render_hint='1d_plot',xlin=False,ylin=False,xlabel='q (A^-1)',ylabel='Intensity (AU)')
     def getReducedData(self,reduce_type='1d',**kwargs):
-    	img = self.getData(**kwargs)
+        start_time = datetime.datetime.now()
+        
+        
+        img = np.array(self.getData(**kwargs))
 
-    	if self.mask is None:
-            mask = np.zeros(np.shape(data))
+        got_image = datetime.datetime.now()
+        
+        if self.mask is None:
+            mask = np.zeros(np.shape(img))
         else:
             mask = self.mask
 
         if reduce_type == '1d':
-        	res = self.integrator.integrate1d(img,self.npts,unit='q_A^-1',mask=mask,error_model='azimuthal')
+            res = self.integrator.integrate1d(img,self.reduction_params['npts'],unit='q_A^-1',mask=mask,error_model='azimuthal')
         elif reduce_type == '2d':
-       		res = self.integrator.integrate2d(img,self.npts,unit='q_A^-1',mask=mask,error_model='azimuthal')
+            res = self.integrator.integrate2d(img,self.reduction_params['npts'],unit='q_A^-1',mask=mask,error_model='azimuthal')    
+            res = res.intensity
         else:
-        	raise ValueError('unsupported return_type')
-
+            raise ValueError('unsupported return_type')
+        reduced_image = datetime.datetime.now()
+        
+        try:
+            self.app.logger.info(f'Reduced an image, image fetch took {got_image - start_time}, reduction took {reduced_image - got_image}')
+        except AttributeError:
+            pass
+        
         return res
 
 

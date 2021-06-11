566ffec59532d775e8b782adb4dc3293748d1314
lizzie <lizziejperez@gmail.com>
2021-06-11 14:44:34 -0400

    Added Buttons to Queue Editor
    Added a "commit changes" button, "move selected to top" button, "move selected to bottom" button, and "remove selected" button.

--- static/queueEditor.js
+++ static/queueEditor.js
@@ -42,7 +42,12 @@ function editQueue(serverKey) {
             console.log(tempTask);
         }
 
-        var editorControls = '<button onclick="closeQueueEditor()" style="float:right;">x</button>';
+        var closeBtn = '<button onclick="closeQueueEditor()" style="float:right;">x</button>';
+        var moveSelectedTopBtn = '<button onclick="">Move Selected Task(s) to Top</button>'; // TODO make the function(s) for the button
+        var moveSelectedBottomBtn = '<button onclick="">Move Selected Task(s) to Bottom</button>'; // TODO make the function(s) for the button
+        var removeSelectedBtn = '<button onclick="">Remove Selected Task(s)</button>'; // TODO make the function(s) for the button
+        var commitBtn = '<button onclick="">Commit Queue Edits</button>'; // TODO make the function(s) for the button
+        var editorControls = moveSelectedTopBtn+moveSelectedBottomBtn+removeSelectedBtn+commitBtn+closeBtn+'<hr>'; 
 
         var tasks = '';
         for(let i in queueTasks) {

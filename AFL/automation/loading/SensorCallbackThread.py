c6e718dbb44f72834432dcf2e30798f575918155
AFL Operations Account <saNistoRoboto@peterbeaucage.com>
2022-07-05 18:52:08 -0500

    Load stopper tweaks

--- AFL/automation/loading/SensorCallbackThread.py
+++ AFL/automation/loading/SensorCallbackThread.py
@@ -8,18 +8,31 @@ import pathlib
 class SensorCallbackThread(threading.Thread):
     def __init__(self,poll,period=0.1,daemon=True,filepath=None):
         threading.Thread.__init__(self, name='CallbackThread', daemon=daemon)
+
+        self.app = None
         
         self.poll = poll
         self.period = period
+
+        self.thread_start = datetime.datetime.now()
         
         self._stop = False
         self._lock = threading.Lock()
         
         self._signal = []
+        self.status_str = 'No action yet...'
 
         if filepath is not None:
             self.filepath = pathlib.Path(filepath)
             self.filepath.mkdir(parents=True, exist_ok=True)
+
+    def update_status(self,value):
+        self.status_str = value
+        if self.app is not None:
+            self.app.logger.info(value)
+        else:
+            print(value)
+
         
     def terminate(self):
         self._stop = True
@@ -28,13 +41,12 @@ class SensorCallbackThread(threading.Thread):
         pass
         
     def run(self):
-        i=0
         print('Starting runloop for CallbackThread:')
         while not self._stop:
-            print(f'Callback iteration {i:010d}',end='\r')
+            dt = datetime.datetime.now()-self.thread_start
+            print(f'Running for {dt.seconds:010d} seconds',end='\r')
             self.process_signal()
             time.sleep(self.period)
-            i+=1
 
 class StopLoadCBv1(SensorCallbackThread):
     def __init__(
@@ -61,19 +73,20 @@ class StopLoadCBv1(SensorCallbackThread):
         self.post_detection_sleep = post_detection_sleep 
         self.timeout = datetime.timedelta(seconds=timeout)
         self.baseline_duration = baseline_duration
+
         
-    def process_signal(self):
+    def process_signal(self): 
         if 'PROGRESS' in getServerState(self.load_client):
-            datestr = datetime.datetime.strftime(datetime.datetime.now(),'%y/%m/%d-%H:%M:%S')
-            print(f'[{datestr}] Detected a load...')
+            datestr = datetime.datetime.strftime(datetime.datetime.now(),'%y%m%d-%H:%M:%S')
+            self.update_status(f'[{datestr}] Detected a load...')
             start = datetime.datetime.now()
-            print(f'Taking baseline data for {self.baseline_duration} s.')
+            self.update_status(f'Taking baseline data for {self.baseline_duration} s.')
             time.sleep(self.baseline_duration)
             
             signal = self.poll.read()
             
             baseline_val = np.mean(signal[-self.threshold_npts:])
-            print(f'Found baseline at {baseline_val}')
+            self.update_status(f'Found baseline at {baseline_val}')
             
             while True and (not self._stop):
                 signal = self.poll.read()
@@ -84,18 +97,93 @@ class StopLoadCBv1(SensorCallbackThread):
                 if (condition1 or condition2) and condition3:
                     time.sleep(self.period)
                 else:
-                    datestr = datetime.datetime.strftime(datetime.datetime.now(),'%y/%m/%d-%H:%M:%S')
+                    datestr = datetime.datetime.strftime(datetime.datetime.now(),'%y%m%d-%H:%M:%S')
                     if condition3:
-                        print(f'[{datestr}] Load timed out')
-
+                        self.update_status(f'[{datestr}] Load stopped at voltage mean = {np.mean(signal[-self.threshold_npts:])} and stdev = {np.std(signal[-self.threshold_npts:])}')
                     else:
-                        print(f'[{datestr}] Load stopped at voltage mean = {np.mean(signal[-self.threshold_npts:])} and stdev = {np.std(signal[-self.threshold_npts:])}')
-                    print(f'Elapsed time: {datetime.datetime.now()-start}')
+                        self.update_status(f'[{datestr}] Load timed out')
+                    self.update_status(f'Elapsed time: {datetime.datetime.now()-start}')
                     time.sleep(self.post_detection_sleep)
                     self.load_client.server_cmd(cmd='stopLoad',secret='xrays>neutrons')
 
-                    filename = self.filepath/'Sensor-'+datestr+'.txt'
-                    print(f'Saving signal data to {filename}')
+                    filename = self.filepath/str('Sensor-'+datestr+'.txt')
+                    self.update_status(f'Saving signal data to {filename}')
+                    np.savetxt(filename,signal)
+
+                    time.sleep(self.loadstop_cooldown)
+                    break
+
+class StopLoadCBv2(SensorCallbackThread):
+    def __init__(
+        self, 
+        poll,
+        period,
+        load_client,
+        threshold_npts = 20,
+        threshold_v_step = 1,
+        threshold_std = 2.5,
+        timeout = 120,
+        min_load_time=30,
+        loadstop_cooldown = 2,
+        post_detection_sleep = 0.2 ,
+        baseline_duration = 10,
+        daemon=True,
+        filepath=None,
+    ):
+        super().__init__(poll=poll,period=period,daemon=daemon,filepath=filepath)
+        self.load_client = load_client
+        self.threshold_npts = threshold_npts
+        self.threshold_v_step = threshold_v_step 
+        self.threshold_std =  threshold_std 
+        self.loadstop_cooldown =  loadstop_cooldown 
+        self.post_detection_sleep = post_detection_sleep 
+        self.min_load_time = datetime.timedelta(seconds=min_load_time)
+        self.timeout = datetime.timedelta(seconds=timeout)
+        self.baseline_duration = baseline_duration
+
+        
+    def process_signal(self):
+        if 'PROGRESS' in getServerState(self.load_client):
+            datestr = datetime.datetime.strftime(datetime.datetime.now(),'%y%m%d-%H:%M:%S')
+            self.update_status(f'[{datestr}] Detected a load...')
+            start = datetime.datetime.now()
+
+            self.poll.reset_load_buffer()
+
+            self.update_status(f'Taking baseline data for {self.baseline_duration} s.')
+            time.sleep(self.baseline_duration)
+            
+            signal = np.array(self.poll.read_load_buffer())
+            
+            baseline_val = np.mean(signal[-self.threshold_npts:,1])#column 0 is microseconds since beginning of load
+            self.update_status(f'Found baseline at {baseline_val}')
+            
+            while True and (not self._stop):
+                signal = np.array(self.poll.read_load_buffer())
+
+                small_v_step = np.abs(np.mean(signal[-self.threshold_npts:,1])-baseline_val) < self.threshold_v_step 
+                large_std = np.std(signal[-self.threshold_npts:,1]) > self.threshold_std
+
+                time_since_load_start = datetime.datetime.now()-start 
+                timed_out = time_since_load_start > self.timeout
+                too_soon = time_since_load_start < self.min_load_time
+                
+                if too_soon:
+                    time.sleep(self.period)
+                elif (small_v_step or large_std) and (not timed_out):
+                    time.sleep(self.period)
+                else:
+                    datestr = datetime.datetime.strftime(datetime.datetime.now(),'%y%m%d-%H:%M:%S')
+                    self.update_status(f'Elapsed time: {datetime.datetime.now()-start}')
+                    if not timed_out:
+                        self.update_status(f'[{datestr}] Load stopped at voltage mean = {np.mean(signal[-self.threshold_npts:,1])} and stdev = {np.std(signal[-self.threshold_npts:,1])}')
+                    else:
+                        self.update_status(f'[{datestr}] Load timed out')
+                    time.sleep(self.post_detection_sleep)
+                    self.load_client.server_cmd(cmd='stopLoad',secret='xrays>neutrons')
+
+                    filename = self.filepath/str('Sensor-'+datestr+'.txt')
+                    # self.update_status(f'Saving signal data to {filename}')
                     np.savetxt(filename,signal)
 
                     time.sleep(self.loadstop_cooldown)
--- server_scripts/LoadStopper.py
+++ server_scripts/LoadStopper.py
@@ -1,32 +1,45 @@
+from AFL.automation.APIServer.APIServer import APIServer
 from AFL.automation.loading.SensorPollingThread import SensorPollingThread
 from AFL.automation.loading.SensorCallbackThread import StopLoadCBv1
 from AFL.automation.loading.LabJackSensor import LabJackSensor
+from AFL.automation.loading.LoadStopperDriver import LoadStopperDriver
 from AFL.automation.APIServer.Client import Client
 import pathlib
 
 
 if __name__=='__main__':
-    load_client = Client('piloader',port=5000)
-    load_client.login('LoadStopper')
-    load_client.pause(False)
 
-    period = 0.05
     sensor = LabJackSensor()
-    poll = SensorPollingThread(sensor,period=period,callback=None,hv_pipe=None,window=1000,filename=None,daemon=True):
-    stopper = StopLoadCBv1( 
-            poll,
-            period=period,
-            load_client=load_client,
-            threshold_npts = 50,
-            threshold_v_step = 1,
-            threshold_std = 3.0,
-            timeout = 120,
-            loadstop_cooldown = 2,
-            post_detection_sleep = 1 ,
-            baseline_duration = 10,
-            daemon=False,
-            filepath=pathlib.Path.home()'/.afl/loadstopper_data/'
-    )
+    driver = LoadStopperDriver(sensor)
 
-    poll.start()
-    stopper.start()
+    server = APIServer('LoadStopper',contact='tyler.martin@nist.gov')
+    server.add_standard_routes()
+    server.create_queue(driver)
+    server.init_logging()#toaddrs=['tbm@nist.gov'])
+    server.run(host='0.0.0.0',port=5051, debug=False)
+
+
+    # load_client = Client('piloader',port=5000)
+    # load_client.login('LoadStopper')
+    # load_client.pause(False)
+
+    # period = 0.05
+    # sensor = LabJackSensor()
+    # poll = SensorPollingThread(sensor,period=period,callback=None,hv_pipe=None,window=1000,filename=None,daemon=True)
+    # stopper = StopLoadCBv1( 
+    #         poll,
+    #         period=period,
+    #         load_client=load_client,
+    #         threshold_npts = 50,
+    #         threshold_v_step = 1,
+    #         threshold_std = 3.0,
+    #         timeout = 120,
+    #         loadstop_cooldown = 2,
+    #         post_detection_sleep = 1 ,
+    #         baseline_duration = 10,
+    #         daemon=False,
+    #         filepath=pathlib.Path.home()/'.afl/loadstopper_data/'
+    # )
+
+    # poll.start()
+    # stopper.start()

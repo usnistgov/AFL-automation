faf7a4dfc7711790e90a40c6261df479417284f2
NIST AFL - device account <saNistoRoboto@peterbeaucage.com>
2022-08-02 17:08:03 +0200

    Update SINQSANS.py
    First tested/working implementation of SINQSANS over Telnet

--- AFL/automation/instrument/SINQSANS.py
+++ AFL/automation/instrument/SINQSANS.py
@@ -17,7 +17,7 @@ class SINQSANS(ScatteringInstrument,Driver):
     defaults['sics_host'] = 'sans.psi.ch'
     defaults['sics_port'] = 1301
     defaults['user_login'] = 'User 22lns1' 
-    defaults['empty transmission'] = None
+    defaults['empty transmission'] = 1
     defaults['transmission strategy'] = 'sum'
     defaults['reduced_data_dir'] = '/mnt/home/chess_id3b/beaucage/211012-data'
     defaults['exposure'] = 1.
@@ -38,8 +38,9 @@ class SINQSANS(ScatteringInstrument,Driver):
         Driver.__init__(self,name='SINQSANS',defaults=self.gather_defaults(),overrides=overrides)
         ScatteringInstrument.__init__(self)
 
-        self.client = SICSClient(self.config['sics_host'],self.config['sics_port'],self.config['user_login'])
-        
+        self.client = SICSTelnetClient(self.config['sics_host'],self.config['sics_port'],self.config['user_login'])
+        self.status_monitor_client =  SICSTelnetClient(self.config['sics_host'],self.config['sics_port'],self.config['user_login'])
+       
         if self.config['reduced_data_dir'] is not None:
             os.chdir(self.config['reduced_data_dir'])
 
@@ -61,65 +62,27 @@ class SINQSANS(ScatteringInstrument,Driver):
         'set_empty_transmission':{'label':'Set Empty Trans?','type':'boolean','default':False}
         }})
     def measureTransmission(self,set_empty_transmission=False,return_full=False):
-        warnings.warn('measureTransmission is ill-defined on instruments with beamstop diodes.  Returning the last measured transmission.  To avoid this warning, call lastTransmission directly.',stacklevel=2)
-        return self.lastTransmission(set_empty_transmission=set_empty_transmission,return_full=return_full)        
-    def lastTransmission(self,set_empty_transmission=False,return_full=False):
-        open_beam = self.client.get_counter(self.config['i0_counter'])
-        trans_beam = self.client.get_counter(self.config['diode_counter'])
-        print(f"lastTransmission: open_beam={open_beam}")
-        print(f"lastTransmission: trans_beam={trans_beam}")
-        
+        self.transmissionMode()
+        self._simple_expose(exposure=2,block=True)
         try:
-            trans = trans_beam / open_beam
-        except ZeroDivisionError:
-            trans=0
- 
+            cts = int(self.client.ask_param('banana sum 40 80 40 80'))
+        except ValueError:
+            cts = int(self.client.ask_param('banana sum 40 80 40 80'))
+        monitor_cts = 20000
+        self.scatteringMode()
+        
+        trans = cts/monitor_cts
         
         if set_empty_transmission:
-            #XXX! Should this be stored in config?
             self.config['empty transmission'] = trans
-             
-            retval = (trans,open_beam,trans_beam,self.config['empty transmission'])
-        elif self.config['empty transmission'] is not None:
-            if return_full:
-                retval = (trans / self.config['empty transmission'],open_beam,trans_beam,self.config['empty transmission'])
-            else:
-                retval = trans / self.config['empty transmission']
-            self.app.logger.info(f'Scaling raw transmission of {trans*100}% using empty transmission of {self.config["empty transmission"]*100} % for reported sample transmission of {trans / self.config["empty transmission"]*100}%')
+        self.last_measured_transmission = (trans,monitor_cts,cts,self.config['empty transmission'])
+        if return_full:
+            return self.last_measured_transmission
         else:
-            if return_full:
-                retval=(trans,open_beam,trans_beam.sum())
-            else:
-                retval = trans
-        self.last_measured_transmission = (trans/self.config['empty transmission'],open_beam,trans_beam,self.config['empty transmission'])
-        self.status_txt = 'Idle'
-        return retval
- 
-    def measureTransmissionQuick(self,exp=0.05,fn='align'):
-            self._simple_expose(exposure=exp,name= fn,block=True)
-            retval = self.lastTransmission()
-            if self.config['open beam intensity'] is not None:
-                retval = retval / self.config['open beam intensity']
-            return retval          
-    
-    def interactiveLoad(self,name='lineup',cutoff_trans = 0.8,timeout=120):
-        self.client.run_cmd('pil_off')
-        self.client.run_cmd('opens')
-        self.client.run_cmd(f'newfile {name}')
-        start = datetime.datetime.now()
-        timeout=datetime.timedelta(seconds=timeout)
-        self.client.run_cmd('ct 0.1')
-        baseline_trans = self.lastTransmission()
-        
-        while(datetime.datetime.now() - start)<timeout:
-            self.client.run_cmd('ct 0.1')
-            trans_ratio = self.lastTransmission()/baseline_trans
+            return trans/self.config['empty transmission']
 
-            if trans_ratio < cutoff_trans_ratio:
-                self.app.logger.debug(f'Stopping load at transmission ratio = {trans_ratio}')
-                break
-        self.client.run_cmd('closes')        
-        self.client.run_cmd('pil_on')
+    def lastMeasuredTransmission(self):
+        return self.last_measured_transmission
         
     @Driver.unqueued()        
     def getExposure(self):
@@ -152,7 +115,7 @@ class SINQSANS(ScatteringInstrument,Driver):
     def setExposure(self,exposure):
         if self.app is not None:
             self.app.logger.debug(f'Setting exposure time to {exposure}')
-        self.app.config['exposure'] = exposure
+        self.config['exposure'] = exposure
 
     def setFilename(self,name):
         if self.app is not None:
@@ -207,49 +170,59 @@ class SINQSANS(ScatteringInstrument,Driver):
                 
         return np.nan_to_num(data)
 
-    def getMotorPosition(self,name):
-        return self.client.get_motor(name).get_position()
 
-    def moveAxis(self,axis,value,block=False):
-        '''
-        Moves a single axis using a connection to a labview vi.
-        @param axis: the axis id or name of the motor to move
-        @param value: the position of the motor to move to
-        @param block: if True, this function will not return until the move is complete.
-        '''
-        mot = self.client.get_motor(axis).move(value)
-    
-        mot.move(value)
-        if block:
-            while(mot.moving):
-                pass
-
-
-    def _simple_expose(self,name=None,exposure=None,block=False):
+    def _simple_expose(self,name=None,exposure=None,aects=1e6,block=False):
         if name is None:
             name=self.getFilename()
         else:
             self.setFilename(name)
 
         if exposure is None:
-            exposure=self.getExposure()
-        else:
-            self.setExposure(exposure)
+            exposure=self.getAutoExposure(desired_counts = aects)
         
-        self.status_txt = f'Starting {exposure} s count named {name}'
+        self.setExposure(exposure)
+        
+        self.status_txt = f'Starting {exposure} moni count named {name}'
         if self.app is not None:
-            self.app.logger.debug(f'Starting exposure with name {name} for {exposure} s')
+            self.app.logger.debug(f'Starting exposure with name {name} for {exposure} moni cts')
 
-
-        self.client.run_cmd(f'opens')
-        
-        self.client.run_cmd(f'tseries 1 {exposure}')
-        self.client.run_cmd(f'closes')
-
+        self.client.send_cmd(f'count moni {self.config["exposure"]}')
         if block:
-            raise NotImplementedError()
-            self.status_txt = 'Accessing Image'
-            return self.getData(lv=lv)
+            time.sleep(2)
+            self.blockForIdle()
+            time.sleep(0.5)
+            while self.client.conn.read_lazy() != b'':
+                pass
+
+    def transmissionMode(self):
+        self.client.send_cmd(f'att 1')
+        time.sleep(0.5)
+        self.blockForIdle()
+        self.client.send_cmd(f'bsout')
+        time.sleep(0.5)
+        self.blockForIdle()
+
+    def scatteringMode(self):
+        self.client.send_cmd(f'bsin')
+        time.sleep(0.5)
+        self.blockForIdle()
+        self.client.send_cmd(f'att 0')
+        time.sleep(0.5)
+        self.blockForIdle()
+
+
+    
+    def getAutoExposure(self,short_count_dur=2,desired_counts = 1000000):
+        self.client.send_cmd(f'count moni {short_count_dur} n')
+        time.sleep(1)
+        self.blockForIdle()
+        self.client.conn.read_lazy()
+        try:
+            counts = int(self.client.ask_param('banana sum 0 127 0 127'))
+        except ValueError:
+            counts = int(self.status_monitor_client.ask_param('banana sum 0 127 0 127'))
+        count_rate = counts / short_count_dur
+        return (desired_counts / count_rate)
 
     @Driver.quickbar(qb={'button_text':'Expose',
         'params':{
@@ -258,85 +231,47 @@ class SINQSANS(ScatteringInstrument,Driver):
         'reduce_data':{'label':'Reduce?','type':'bool','default':True},
         'measure_transmission':{'label':'Measure Trans?','type':'bool','default':True}
         }})
-    def expose(self,name=None,exposure=None,nexp=1,block=True,reduce_data=True,measure_transmission=True,save_nexus=True):
+    def expose(self,name=None,exposure=None,block=True,reduce_data=True,measure_transmission=True):
         if name is None:
             name=self.getFilename()
         else:
             self.setFilename(name)
-
+      
+        if measure_transmission:
+            self.measureTransmission() 
+        
         if exposure is None:
-            exposure=self.getExposure()
-        else:
-            self.setExposure(exposure)
+            exposure=self.getAutoExposure()
+        self.setExposure(exposure)
         
-        self.status_txt = f'Starting {exposure} s count named {name}'
-        if self.app is not None:
-            self.app.logger.debug(f'Starting exposure with name {name} for {exposure} s')
-        
-        pos_trans = []
-        pos_raw_data = []
-        pos_simple_trans = []
-        pos_reduced_data = []
+        self.scatteringMode()
 
-        for idx,pos in enumerate(self.config['measurement_positions']):
-            self.client.run_cmd(f'umv samx {pos}')
-            self.client.run_cmd(f'opens')
-            self.client.run_cmd(f'tseries {nexp} {exposure/10}')
-            self.client.run_cmd(f'tseries {nexp} {exposure}',block=True)
-            self.client.run_cmd(f'closes')
-            
-            pos_raw_data.append(self.getData())
-            pos_reduced_data.append(self.getReducedData(write_data=True,filename=f'{name}_{idx}'))
-            pos_trans.append(self.lastTransmission(return_full=True))
-            pos_simple_trans.append(pos_trans[-1][0])
-                
-        #time.sleep(0.5)
+        self.status_txt = f'Starting {exposure} moni count named {name}'
+        if self.app is not None:
+            self.app.logger.debug(f'Starting exposure with name {name} for {exposure} moni cts')
+
+        self.client.send_cmd(f'count moni {self.config["exposure"]}')
         if block or reduce_data or save_nexus:
-            self.client.block_for_ready()
-            self.status_txt = 'Accessing Image'
-            self.app.logger.debug(f'Min transmission was {np.min(pos_simple_trans)} at index {np.argmin(pos_simple_trans)}; returning that data.  Other transmissions were {pos_simple_trans}')
-            data = pos_raw_data[np.argmin(pos_simple_trans)]
-            transmission = pos_trans[np.argmin(pos_simple_trans)]
-            #transmission = self.lastTransmission(return_full=True)
-            if save_nexus:
-                self.status_txt = 'Writing Nexus'
-                self._writeNexus(data,name,name,transmission)
+            time.sleep(2)
+            self.blockForIdle()
             if reduce_data:
                 self.status_txt = 'Reducing Data'
-                reduced = pos_reduced_data[np.argmin(pos_simple_trans)]
-                print(np.shape(reduced))
+                reduced = self.getReducedData(write_data=True,filename=name)
                 np.savetxt(f'{name}_chosen_r1d.csv',np.transpose(reduced),delimiter=',')
-                #self.getReducedData(write_data=True,filename=name)
-                #if save_nexus:
-                    #self._appendReducedToNexus(reduced,name,name)
             self.status_txt = 'Instrument Idle'
             return transmission
-    def scan(self,axis,npts,start,step,name=None,exposure=None,block=False):
-        if name is not None:
-            self.setFilename(name)
-        else:
-            name=self.getFilename()
+    
+    def blockForIdle(self):
+        self.status_monitor_client.success()
 
-        if exposure is not None:
-            self.setExposure(exposure)
-        else:
-            exposure=self.getExposure()
+    def getStatus(self):
+        status = self.status_monitor_client.ask_param('el737 RS')
+        return status
 
-        self.setNScans(npts)
-        self.setSweepAxis(axis)
-        self.setSweepStart(start)
-        self.setSweepStep(step)
-            
-        if self.app is not None:
-            self.app.logger.debug(f'Starting exposure with name {name} for {exposure} s')
+    def isCounting(self):
+        return bool(int(self.getStatus()))
 
-        with LabviewConnection() as lv:
-            lv.main_vi.setcontrolvalue('Expose Pilatus',True)
-        time.sleep(0.5)
-        if block:
-            while(self.getStatus() != 'Success'):
-                time.sleep(0.1)
-                
+
                
     def status(self):
         status = []
@@ -365,13 +300,26 @@ class SICSTelnetClient():
         if resp[0:2] != 'OK':
             raise Exception(f'received unexpected answer from SICS: {resp}')
 
-    def ask_param(self,param):
+    def ask_param(self,param,second_try = False):
         cmd = f'{param}\r\n'.encode('utf-8')
         self.conn.write(cmd)
         time.sleep(1)
         response = self.conn.read_very_eager().decode()
-        regexsplit = re.findall(r'(.*?) = (.*?)\r\n',response)
-        return regexsplit[0][1]
+        if 'ERROR: Busy' in response:
+            time.sleep(1)
+            print('rcvd busy response; retrying')
+            return self.ask_param(param)
+
+        try:
+            regexsplit = re.findall(r'(.*?) = (.*?)\r\n',response)
+            retval = regexsplit[0][1]
+        except IndexError:
+            self.conn.read_lazy()
+            if second_try:
+                raise IndexError(f'weird answer from server: {response}, cannot parse after 2 tries.') 
+            time.sleep(1) #cool down incase you were just too fast
+            retval = self.ask_param(param,second_try = True)    
+        return retval
 
     def set_param(self,param,val):
         cmd = f'{param} {val}\r\n'.encode('utf-8')
@@ -379,5 +327,15 @@ class SICSTelnetClient():
         response = self.conn.read_very_eager()
         return response.decode().replace(r'\r\n','')
 
+    def send_cmd(self,cmd):
+        cmd = f'{cmd}\r\n'.encode('utf-8')
+        self.conn.write(cmd)
+        response = self.conn.read_very_eager().decode()
+        return response
+    def success(self):
+        cmd = f'success\r\n'.encode('utf-8')
+        self.conn.write(cmd)
+        response = self.conn.read_until(b'\r\n').decode()
+        return response
     def __del__(self):
         self.conn.close()

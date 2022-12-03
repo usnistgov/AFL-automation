ea054c8b0254eb0b311427f45b63531718df21a4
saNistoRoboto <saNistoRoboto@nist.gov>
2022-12-03 16:18:40 +0000

    bugfix: fixing shaker communication

--- AFL/automation/prepare/OT2_Driver.py
+++ AFL/automation/prepare/OT2_Driver.py
@@ -180,7 +180,7 @@ class OT2_Driver(Driver):
 
     def latch_shaker(self):
         with serial.Serial(self.config['shaker_port'],115200) as p:
-            p.write(f'M243 S{str(int(temp))}\r\n'.encode())
+            p.write(f'M243\r\n'.encode())
 
     def get_shaker_temp(self):
         with serial.Serial(self.config['shaker_port'],115200) as p:
@@ -196,7 +196,7 @@ class OT2_Driver(Driver):
 
     def get_shake_latch_status(self):
         with serial.Serial(self.config['shaker_port'],115200) as p:
-            p.write(f'M241 S{str(int(rpm))}\r\n'.encode())
+            p.write(f'M241\r\n'.encode())
             resp = p.readline()
         return resp
 

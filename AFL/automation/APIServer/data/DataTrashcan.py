1e714638d3f038420d6be83c0cfdaa2b44e6603f
Peter Beaucage <peter.beaucage@nist.gov>
2023-04-18 14:45:14 -0400

    Create DataTrashcan.py

---
+++ AFL/automation/APIServer/data/DataTrashcan.py
@@ -0,0 +1,6 @@
+class DataTrashcan(DataPacket):
+'''
+  A DataPacket implementation *for testing only* that takes all its data and simply throws it away.
+'''
+    def transmitData(self):
+        pass

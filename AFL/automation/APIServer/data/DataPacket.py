e49fd7e41603a8f3063303a281e94ccab48a316a
Peter Beaucage <peter.beaucage@nist.gov>
2023-04-18 14:40:47 -0400

    add transient/sample/system hierarchy

--- AFL/automation/APIServer/data/DataPacket.py
+++ AFL/automation/APIServer/data/DataPacket.py
@@ -1,17 +1,50 @@
 class DataPacket:
-
-    def __init__(self):
-        self._dict = {}
+    
+    PROTECTED_SYSTEM_KEYS = [
+        'driver_name',
+        'driver_config',
+        'platform_serial',
         
+        
+    
+    ]
+    PROTECTED_SAMPLE_KEYS = [
+        'sample_name',
+        'sample_uuid',
+        'sample_composition',
+        'sample_al_components',
+        
+    
+    ]
+    
+    def __init__(self):
+        self._transient_dict = {}
+        self._system_dict = {}
+        self._sample_dict = {}
         
     def __getitem__(self,key):
-        return self._dict[key]
+        if key in self._system_dict.keys():
+            return self._system_dict[key]
+        elif key in self._sample_dict.keys():
+            return self._sample_dict[key]
+        else:
+            return self._transient_dict[key]
     
     def __setitem__(self,key,value):
-        self._dict[key] = value
+        if key in PROTECTED_SYSTEM_KEYS:
+            self._system_dict[key] = value
+        elif key in PROTECTED_SAMPLE_KEYS:
+            self._sample_dict[key] = value
+        else:
+            self._transient_dict[key] = value
     
+    def resetClass(self):
+        self._transient_dict = {}
+        
+        
     def setupDefaults(self):
-   
+        pass
+        
     def finalizeData(self):
         raise NotImplementedError
         

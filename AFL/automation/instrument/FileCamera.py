7f142e0574ed726777fe2d05a3b4bb79dc3421bf
NIST AFL - device account <saNistoRoboto@peterbeaucage.com>
2023-05-29 15:15:10 +0200

    Initial add of camera-based turbidimeter

---
+++ AFL/automation/instrument/FileCamera.py
@@ -0,0 +1,16 @@
+import requests, io
+import numpy as np
+from PIL import Image
+
+
+def FileCamera:
+    
+    def __init__(self):
+        pass
+        
+    def collect(self,fname):
+        '''
+        
+        '''
+        
+        return np.array(Image.open(fname))

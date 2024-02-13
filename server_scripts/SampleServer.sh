2ca5f9dbef605c5e7313dc80a9a4c35b344045cd
tbm <tyler.martin@nist.gov>
2024-02-13 22:15:12 +0000

    [feat] adds SampleServer server_scripts

--- server_scripts/SampleServer.sh
+++ server_scripts/SampleServer.sh
@@ -4,7 +4,20 @@ git config --global credential.helper store
 
 conda activate afl_agent
 
+if [[ -z "${TILED_API_KEY}" ]]; then
+  export TILED_API_KEY=$(cat ~/.afl/tiled_api_key)
+else
+  export TILED_API_KEY="${TILED_API_KEY}"
+fi
+
+if [[ -z "${AFL_SYSTEM_SERIAL}" ]]; then
+  export AFL_SYSTEM_SERIAL=$(cat ~/.afl/system_serial)
+else
+  export AFL_SYSTEM_SERIAL="${AFL_SYSTEM_SERIAL}"
+fi
+
+
 cd ~/AFL-automation/
 git pull
 
-python ~/AFL-automation/server_scripts/SampleServer_USAXS.py
+python ~/AFL-automation/server_scripts/SampleServer.py

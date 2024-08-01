04ce3c26937b1aa6309d30bcc16b3c63bb5ff84b
Peter Beaucage <peter.beaucage@nist.gov>
2024-08-01 16:24:19 +0000

    Add support for driver object hierarchies.

--- AFL/automation/shared/launcher.py
+++ AFL/automation/shared/launcher.py
@@ -11,6 +11,17 @@ from AFL.automation.APIServer.data.DataTiled import DataTiled
 
 from AFL.automation.shared.PersistentConfig import PersistentConfig
 
+
+try:
+    main_module_name = _override_main_module_name_
+except NameError:
+    import __main__
+    main_module_fullpath = os.path.abspath(__main__.__file__)
+    main_module_name = os.path.basename(main_module_fullpath).replace('.py','')
+driver_module = importlib.import_module(main_module_name,'')
+driver_name = driver_module.__name__.split('.')[-1]
+driver_cls = getattr(driver_module,driver_name)
+
 AFL_GLOBAL_CONFIG = PersistentConfig(
         os.path.join(os.path.expanduser('~'),'.afl','config.json'),
         defaults = {
@@ -20,19 +31,19 @@ AFL_GLOBAL_CONFIG = PersistentConfig(
         'tiled_api_key': '',
         'bind_address': '0.0.0.0',
         'ports': {},
+        'driver_custom_configs': {}
         },
         max_history=100,
 )
-
 try:
-    main_module = _override_main_module_
+        to_maybe_update = _DEFAULT_CUSTOM_CONFIG 
+        # if this driver has not provided a default custom config, we simply throw a NameError
+        # and move on
+        if driver_name not in AFL_GLOBAL_CONFIG['driver_custom_configs'].keys():
+                # if there is already global config for this driver, do nothing, otherwise...
+                AFL_GLOBAL_CONFIG['driver_custom_configs'] = _DEFAULT_CUSTOM_CONFIG
 except NameError:
-    import __main__
-    main_module_fullpath = os.path.abspath(__main__.__file__)
-    main_module_name = os.path.basename(main_module_fullpath).replace('.py','')
-driver_module = importlib.import_module(main_module_name,'')
-driver_cls = getattr(driver_module,driver_module.__name__.split('.')[-1])
-
+        pass
 if 'AFL_SYSTEM_SERIAL' not in os.environ.keys():
         os.environ['AFL_SYSTEM_SERIAL'] = AFL_GLOBAL_CONFIG['system_serial']
 
@@ -41,7 +52,44 @@ if main_module_name in AFL_GLOBAL_CONFIG['ports'].keys():
 else:
         server_port=5000
 
-driver = driver_cls()
+
+def _reconstitute_objects(obj_dict):
+        if '_classname' not in obj_dict.keys():
+                return obj_dict
+        class_to_make = obj_dict.pop('_classname')
+        class_module_name = '.'.join(class_to_make.split('.')[:-1])
+        class_module = importlib.import_module(class_module_name,'')
+        cls_name = class_to_make.split('.')[:-1]
+        cls_obj = getattr(class_module, cls_name)
+        if '_args' in obj_dict.keys():
+                _args = obj_dict.pop('_args')
+        else:
+                _args = []
+        args = []
+        for item in _args:
+                args.append(_reconstitute_objects(item))
+        kwargs = {}
+        for k,v in obj_dict.items():
+                kwargs[k] = _reconstitute_objects(v)
+
+        return cls_obj(*args,**kwargs)
+
+'''
+        example input for driver_custom_configs:
+
+        'PneumaticPressureLoader':
+                '_classname': 'AFL.automation.loading.PneumaticPressureLoader.PneumaticPressureLoader',
+                'p_ctrl': { '_classname': 'AFL.automation.loading.DigitalOutPressureController.DigitalOutPressureController',
+                            'dig_out': {
+                                 '_classname': 'AFL.automation.loading.LabJackDigitalOut',
+                                 'port': 'DIO1'}}}
+
+
+'''
+if main_module_name in AFL_GLOBAL_CONFIG['driver_custom_configs']:
+        driver = _reconstitute_objs(AFL_GLOBAL_CONFIG['driver_custom_configs'][main_module_name])
+else:
+        driver = driver_cls()
 if len(AFL_GLOBAL_CONFIG['tiled_server'])>0:
         data = DataTiled(AFL_GLOBAL_CONFIG['tiled_server'],
                 api_key = AFL_GLOBAL_CONFIG['tiled_api_key'],

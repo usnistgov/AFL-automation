887be42d9ea6e6850ba0e40726a607162acd2da9
afl_preplab <afl642@nist.gov>
2025-06-05 15:55:57 -0400

    Residual stuff on afl-preplab

--- AFL/automation/sample/CastingServer.py
+++ AFL/automation/sample/CastingServer.py
@@ -30,7 +30,7 @@ class CastingServer(Driver):
         self.name = 'CastingServer_SampleDriver'
 
         if not (len(prep_url.split(':'))==2):
-            raise ArgumentError('Need to specify both ip and port on prep_url')
+            raise ValueError('Need to specify both ip and port on prep_url')
 
         #prepare samples
         self.prep_client = OT2Client(prep_url.split(':')[0],port=prep_url.split(':')[1])
@@ -72,7 +72,8 @@ class CastingServer(Driver):
 
     def update_status(self,value):
         self.status_str = value
-        self.app.logger.info(value)
+        if self.app is not None:
+            self.app.logger.info(value)
     
     def log_event(self,manifest_key,event,write=True):
         #event['started'] = datetime.datetime.strptime(event['started'],'%m/%d/%y %H:%M:%S-%f')
@@ -80,7 +81,9 @@ class CastingServer(Driver):
         
         ds_event = xr.Dataset()
         for k,v in event.items():
-            ds_event[k] = ('event',v)
+            if k=='mix_before' or k=='mix_after':
+                v = str(v)
+            ds_event[k] = ('event',[v])
         
         self.manifests[manifest_key] = xr.concat([
             self.manifests[manifest_key],
@@ -93,7 +96,7 @@ class CastingServer(Driver):
             self.manifests[manifest_key].to_netcdf(self.config[manifest_key])
             self.manifests[manifest_key].to_pandas().to_csv(self.config[manifest_key].replace('nc','csv'))
             
-    def assign_targets(self,protocols,target_map):
+    def assign_targets(self,protocols,target_map=None):
         if target_map is None:
             self.targets = set()
             for name,protocol in protocols.items():
@@ -105,8 +108,10 @@ class CastingServer(Driver):
     
             self.target_map = {}
             for t in self.targets:
-                prep_target = self.prep_client.enqueue(task_name='get_prep_target',interactive=True)['return_val']
-                self.target_map[t] = prep_target
+                response = self.prep_client.enqueue(task_name='get_prep_target',interactive=True)
+                if isinstance(response, dict) and 'return_val' in response:
+                    prep_target = response['return_val']
+                    self.target_map[t] = prep_target
         else:
             self.target_map = target_map
 
@@ -128,13 +133,16 @@ class CastingServer(Driver):
         
         for sample_name,task in zip(sample_names,protocol):
             self.update_status(f'Transferring {task["volume"]} uL from {task["source"]} to {task["dest"]}')
-            self.last_prep = self.prep_client.transfer(interactive=True,**task)
-            self.last_prep_time = self.last_prep['ended'] 
+            response = self.prep_client.transfer(interactive=True,**task)
+            self.last_prep = {}
+            if isinstance(response, dict):
+                self.last_prep.update(response)
+                self.last_prep_time = response.get('ended')
             
             self.last_prep['event_type'] = 'prep'
             self.last_prep['sample_name'] = sample_name
             self.last_prep['stock_name'] = stock_name
-            self.last_prep.update(**task)
+            self.last_prep.update(task)
             self.log_event('manifest_prep',self.last_prep)
             
     def bulk_cast_films(self,**spec):
@@ -150,13 +158,16 @@ class CastingServer(Driver):
         
         for sample_name,plate_name,task in zip(sample_names,plate_names,protocol):
             self.update_status(f'Transferring {task["volume"]} uL from {task["source"]} to {task["dest"]}')
-            self.last_prep = self.prep_client.transfer(interactive=True,**task)
-            self.last_prep_time = self.last_prep['ended'] 
+            response = self.prep_client.transfer(interactive=True,**task)
+            self.last_prep = {}
+            if isinstance(response, dict):
+                self.last_prep.update(response)
+                self.last_prep_time = response.get('ended')
             
             self.last_prep['event_type'] = 'cast'
             self.last_prep['sample_name'] = sample_name
             self.last_prep['plate_name'] = plate_name
-            self.last_prep.update(**task)
+            self.last_prep.update(task)
             self.log_event('manifest_cast',self.last_prep)
 
     def prepare_and_cast(self,**sample):
@@ -164,65 +175,83 @@ class CastingServer(Driver):
         
         sample_name = sample['sample_name']
         plate_names = sample['plate_names']
-        #stock_name = sample['stock_name']
-        # protocols = self.assign_targets({k:sample[k] for k in ['prep_protocol','cast_protocol']})
-        protocols = {k:sample[k] for k in ['prep_protocol','cast_protocol']}
+        min_mix_time = sample.get('min_mix_time', 0)  # Get min_mix_time from sample dict, default to 0
+        protocols = self.assign_targets({k:sample[k] for k in ['prep_protocol','cast_protocol']})
 
         self.update_status(f'Preparing sample: {sample_name}')
         for task in protocols['prep_protocol']:
             self.update_status(f'Transferring {task["volume"]} uL from {task["source"]} to {task["dest"]}')
-            self.last_prep = self.prep_client.transfer(interactive=True,**task)
-            self.last_prep_time = self.last_prep['ended'] 
+            response = self.prep_client.transfer(interactive=True,**task)
+            self.last_prep = {}
+            if isinstance(response, dict):
+                # self.last_prep.update(response)
+                self.last_prep_time = response.get('ended')
+                self.last_prep['queued'] = response.get('queued')
+                self.last_prep['started'] = response.get('started')
+                self.last_prep['ended'] = response.get('ended')
             
             self.last_prep['event_type'] = 'prep'
             self.last_prep['sample_name'] = sample_name
-            #self.last_prep['stock_name'] = stock_name
-            self.last_prep.update(**task)
-            #self.log_event('manifest_prep',self.last_prep)
+            self.last_prep['min_mix_time'] = datetime.timedelta(seconds=min_mix_time)
+            self.last_prep['actual_mix_time'] = datetime.timedelta(seconds=0)
+            self.last_prep.update(task)
+            self.log_event('manifest',self.last_prep)
             
         # shouldn't have to wait, but let's do this for safety
         if self.prep_uuid is not None: 
             self.prep_client.wait(self.prep_uuid)
 
-        # if 'min_mix_time' in sample:
-        #     try:
-        #         if not (len(sample['min_mix_time'])==len(sample['cast_protocol'])):
-        #             raise ValueError(f'min_mix_time has multiple values, but does not match length of cast protocol len(sample["cast_protocol"]')
-        #     except TypeError:
-        #         #value is scalar, make vector
-        #         sample['min_mix_time'] = [sample['min_mix_time']]*len(sample['cast_protocol'])
-        # else:
-        #     sample['min_mix_time'] = [0.0]*len(sample['cast_protocol'])
-        #     
+        # Add mixing wait time if specified
+        if min_mix_time > 0:
+            mix_start_time = datetime.datetime.now()
+            self.update_status(f'Waiting {min_mix_time} seconds for mixing of sample: {sample_name}')
+            time.sleep(min_mix_time)
+            mix_end_time = datetime.datetime.now()
+            actual_mix_time = (mix_end_time - mix_start_time).total_seconds()
+            
+            # Log the mixing event
+            mix_event = {
+                'event_type': 'mix',
+                'sample_name': sample_name,
+                'min_mix_time': datetime.timedelta(seconds=min_mix_time),
+                'actual_mix_time': datetime.timedelta(seconds=actual_mix_time),
+                'started': mix_start_time,
+                'ended': mix_end_time,
+                'queued': mix_start_time
+            }
+            self.log_event('manifest', mix_event)
 
         self.update_status(f'Casting sample: {sample_name}')
+        last_plate_name = None
         for plate_name,task in zip(plate_names,protocols['cast_protocol']):
+            response = self.prep_client.transfer(**task,interactive=True)
+            self.last_prep = {}
+            if isinstance(response, dict):
+                # self.last_prep.update(response)
+                self.last_prep['queued'] = response.get('queued')
+                self.last_prep['started'] = response.get('started')
+                self.last_prep['ended'] = response.get('ended')
             
-            # delta = datetime.timedelta(seconds=min_mix_time)
-            # self.update_status(f'Waiting for min_mix_time to be satisfied: {min_mix_time} s')
-            # while (datetime.datetime.now()-self.last_prep['ended'])<delta:
-            #     time.sleep(0.05)
-            # actual_mix_time = datetime.datetime.now()-self.last_prep['ended']
-                
-            self.update_status(f'Transferring {task["volume"]} uL from {task["source"]} to {task["dest"]}')
-            self.last_prep = self.prep_client.transfer(**task,interactive=True)
-
+            
             self.last_prep['event_type'] = 'cast'
             self.last_prep['sample_name'] = sample_name
             self.last_prep['plate_name'] = plate_name
-            self.last_prep.update(**task)
-            #self.log_event('manifest_cast',self.last_prep)
-            self.update_status(f'Cast {self.last_prep["sample_name"]}!')
+            self.last_prep.update(task)
+            self.log_event('manifest',self.last_prep)
+            self.update_status(f'Cast {sample_name}!')
+            last_plate_name = plate_name
             
-        
-        self.update_status(f'All done for sample {sample_name} on plate {plate_name}!')
+        self.update_status(f'All done for sample {sample_name} on plate {last_plate_name}!')
    
 
 
 
 _DEFAULT_CUSTOM_CONFIG = {
         '_classname': 'AFL.automation.sample.CastingServer.CastingServer',
-        'prep_url': 'localhost:5000'
+        'prep_url': 'localhost:5000',
+        'manifest': '/home/afl642/',
+        'manifest_prep': '/home/afl642/',
+        'manifest_cast': '/home/afl642/'
 }
 _DEFAULT_CUSTOM_PORT = 5000
 if __name__ == '__main__':
